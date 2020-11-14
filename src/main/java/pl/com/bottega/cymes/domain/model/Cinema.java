4ea71384710514fb4c0d559b48e7fcd3432cc62e
Maciej Zubala <maciej.zubala@allegro.pl>
2020-11-14 19:15:47 +0100

    removed port

--- src/main/java/pl/com/bottega/cymes/domain/model/Cinema.java
+++ src/main/java/pl/com/bottega/cymes/domain/model/Cinema.java
@@ -2,15 +2,23 @@ package pl.com.bottega.cymes.domain.model;
 
 import lombok.NonNull;
 import lombok.Value;
+import org.apache.commons.lang3.StringUtils;
 
 import java.util.UUID;
 
 @Value
 public class Cinema {
-    @NonNull
     UUID id;
-    @NonNull
     String city;
-    @NonNull
     String name;
+
+    public Cinema(@NonNull UUID id, @NonNull String city, @NonNull String name) {
+        this.id = id;
+        this.city = withoutSpaces(city);
+        this.name = withoutSpaces(name);
+    }
+
+    private String withoutSpaces(String input) {
+        return StringUtils.join(input.trim().split("\\s+"), " ");
+    }
 }
