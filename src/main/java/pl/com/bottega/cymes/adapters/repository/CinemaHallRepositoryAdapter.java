54c32da8cc991c8574a7fdf93d1e79f5a97f3f08
Maciej Zubala <maciej.zubala@allegro.pl>
2020-11-14 19:53:50 +0100

    after merge fixes

--- src/main/java/pl/com/bottega/cymes/adapters/repository/CinemaHallRepositoryAdapter.java
+++ src/main/java/pl/com/bottega/cymes/adapters/repository/CinemaHallRepositoryAdapter.java
@@ -1,12 +1,30 @@
 package pl.com.bottega.cymes.adapters.repository;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.Repository;
 import org.springframework.stereotype.Component;
 import pl.com.bottega.cymes.domain.model.CinemaHall;
 import pl.com.bottega.cymes.domain.model.CinemaHall.CinemaHallId;
 import pl.com.bottega.cymes.domain.ports.CinemaHallRepository;
 
+import javax.persistence.CascadeType;
+import javax.persistence.Embeddable;
+import javax.persistence.EmbeddedId;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.OrderColumn;
+import javax.persistence.Table;
+import java.io.Serializable;
 import java.util.List;
 import java.util.UUID;
+import java.util.stream.Collectors;
+
+import static pl.com.bottega.cymes.domain.model.CinemaHall.*;
 
 @Component
 public class CinemaHallRepositoryAdapter implements CinemaHallRepository {
@@ -60,7 +78,7 @@ class CinemaHallEntity {
 @Table(name = "rows")
 class RowEntity {
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String layout;
 
