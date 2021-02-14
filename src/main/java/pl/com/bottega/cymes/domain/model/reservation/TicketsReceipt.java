c5ad6c4a4110be93390ecaf4fb4b73f1ed524b18
Maciej Zubala <maciej.zubala@allegro.pl>
2021-02-14 17:41:32 +0100

    more unit tests

--- src/main/java/pl/com/bottega/cymes/domain/model/reservation/TicketsReceipt.java
+++ src/main/java/pl/com/bottega/cymes/domain/model/reservation/TicketsReceipt.java
@@ -13,11 +13,18 @@ public class TicketsReceipt {
     List<TicketsReceiptItem> items;
 
     public TicketsReceipt(List<TicketsReceiptItem> items) {
+        if(uniqueItemsCount(items) != items.size()) {
+            throw new IllegalArgumentException("Duplicate receipt items");
+        }
         this.items = new LinkedList<>(items);
     }
 
     public Money getTotal() {
         return items.stream().map(TicketsReceiptItem::getTotal).reduce(Money.ZERO, Money::add);
+    }
+
+    private long uniqueItemsCount(List<TicketsReceiptItem> items) {
+        return items.stream().map(TicketsReceiptItem::getTicketKind).distinct().count();
     }
 
     @Value
--- src/test/java/pl/com/bottega/cymes/domain/model/reservation/TicketsReceiptTest.java
+++ src/test/java/pl/com/bottega/cymes/domain/model/reservation/TicketsReceiptTest.java
@@ -1,4 +1,32 @@
 package pl.com.bottega.cymes.domain.model.reservation;
 
+import org.junit.jupiter.api.Test;
+import pl.com.bottega.cymes.domain.model.pricing.Money;
+import pl.com.bottega.cymes.domain.model.pricing.TicketKind;
+import pl.com.bottega.cymes.domain.model.reservation.TicketsReceipt.TicketsReceiptItem;
+
+import java.util.List;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+
 public class TicketsReceiptTest {
+    @Test
+    public void returnsCorrectTotal() {
+        TicketsReceipt ticketsReceipt = new TicketsReceipt(List.of(
+                new TicketsReceiptItem(TicketKind.REGULAR, 1, Money.of(20)),
+                new TicketsReceiptItem(TicketKind.FAMILY, 4, Money.of(10)),
+                new TicketsReceiptItem(TicketKind.SENIOR, 1, Money.of(5))
+        ));
+
+        assertThat(ticketsReceipt.getTotal()).isEqualTo(Money.of(65));
+    }
+
+    @Test
+    public void doesNotAllowDuplicateTicketKinds() {
+        assertThatThrownBy(() -> new TicketsReceipt(List.of(
+                new TicketsReceiptItem(TicketKind.REGULAR, 1, Money.of(20)),
+                new TicketsReceiptItem(TicketKind.REGULAR, 4, Money.of(10))
+        ))).isInstanceOf(IllegalArgumentException.class);
+    }
 }
